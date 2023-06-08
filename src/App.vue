b7ec08ab129649cbebec6991553a960bcd904038
LianQi-Kevin <18201584863@163.com>
2023-06-08 12:34:18 +0800

    * First commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Login from "@/view/Login.vue";
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="root">
+<!--      <hello-world msg="Hello"></hello-world>-->
+      <Login></Login>
+  </div>
+</template>
+
+<style scoped>
+div.root {
+    background: gray;
+    width: 100vh;
+    height: 100vh;
+}
+</style>
---
+++ src/view/Login.vue
@@ -0,0 +1,51 @@
+<script setup>
+</script>
+<template>
+    <div class="container">
+        <el-row>
+            <el-col :span="24"><div class="grid-content ep-bg-purple-dark" /></el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="12"><div class="grid-content ep-bg-purple" /></el-col>
+            <el-col :span="12"><div class="grid-content ep-bg-purple-light" /></el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="8"><div class="grid-content ep-bg-purple" /></el-col>
+            <el-col :span="8"><div class="grid-content ep-bg-purple-light" /></el-col>
+            <el-col :span="8"><div class="grid-content ep-bg-purple" /></el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
+            <el-col :span="6"><div class="grid-content ep-bg-purple-light" /></el-col>
+            <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
+            <el-col :span="6"><div class="grid-content ep-bg-purple-light" /></el-col>
+        </el-row>
+        <el-row :gutter="20">
+            <el-col :span="4"><div class="grid-content ep-bg-purple" /></el-col>
+            <el-col :span="4"><div class="grid-content ep-bg-purple-light" /></el-col>
+            <el-col :span="4"><div class="grid-content ep-bg-purple" /></el-col>
+            <el-col :span="4"><div class="grid-content ep-bg-purple-light" /></el-col>
+            <el-col :span="4"><div class="grid-content ep-bg-purple" /></el-col>
+            <el-col :span="4"><div class="grid-content ep-bg-purple-light" /></el-col>
+        </el-row>
+    </div>
+</template>
+
+<style lang="scss">
+.el-row {
+    margin-bottom: 20px;
+}
+.el-row:last-child {
+    margin-bottom: 0;
+}
+.el-col {
+    border-radius: 4px;
+}
+
+.grid-content {
+    background: white;
+    border-radius: 4px;
+    min-height: 36px;
+    min-width: 36px;
+}
+</style>
