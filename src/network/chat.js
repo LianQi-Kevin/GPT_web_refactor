c593710b62440ff81f9d609bf70d3c49ee9de6f6
LianQi-Kevin <18201584863@163.com>
2023-08-23 02:40:36 +0800

    * dev upgrade, ChatGPT to ChatGLM

--- src/network/chat.js
+++ src/network/chat.js
@@ -1,9 +1,9 @@
 import LocalAxios from "./basicAPI.js"
 import {ElMessage} from "element-plus";
-import {accessTokenAdd} from "@/network/interceptors.js";
+// import {accessTokenAdd} from "@/network/interceptors.js";
 
 const chatAxios = new LocalAxios()
-chatAxios.interceptors.request.use(accessTokenAdd)
+// chatAxios.interceptors.request.use(accessTokenAdd)
 
 export async function getChatGPTResponse(conversations, configs) {
     try{
@@ -26,7 +26,8 @@ export async function getChatGPTResponse(conversations, configs) {
 
         // 创建请求
         const chatGPTResponse = await chatAxios.post(
-            '/chat/azure', JSON.stringify(requestJson)
+            // '/chat/azure', JSON.stringify(requestJson)
+            '/chat', JSON.stringify(requestJson)
         )
 
         return {type: 'success', value: chatGPTResponse.data["data"]}
